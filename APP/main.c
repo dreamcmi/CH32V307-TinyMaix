79f524144c91aa688434cf5b5d1c4f0f289ee752
dreamcmi <1912544842@qq.com>
2022-09-09 21:55:54 +0800

    update support for vww and improve the testing process

--- APP/main.c
+++ APP/main.c
@@ -1,32 +1,14 @@
 #include "debug.h"
 #include "tinymaix.h"
+
+
+#define TEST_MNIST (1)
+// #define TEST_CIFAR (1)
+// #define TEST_VWW (1)
+
+#if TEST_MNIST
 #include "mnist_valid_q.h"
-// #include "cifar10_q.h"
-// #include "mbnet96_0.25_q.h"
-
-
-volatile uint32_t g_us_tick;
-void TIM6_Init(void)  // 1us
-{
-    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure = {0};
-
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
-
-    TIM_TimeBaseStructure.TIM_Period = 1;
-    TIM_TimeBaseStructure.TIM_Prescaler = 144 - 1;
-    TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-
-    TIM_TimeBaseInit(TIM6, &TIM_TimeBaseStructure);
-    TIM_ITConfig(TIM6, TIM_IT_Update, ENABLE);
-    TIM_ClearITPendingBit(TIM6, TIM_IT_Update);
-    TIM_Cmd(TIM6, ENABLE);
-    NVIC_SetPriority(TIM6_IRQn, 0x80);
-    NVIC_EnableIRQ(TIM6_IRQn);
-}
-
-
-uint8_t mnist_pic[28*28]={
+uint8_t pic[28*28]={
   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
@@ -56,6 +38,48 @@ uint8_t mnist_pic[28*28]={
   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
 };
+#define IMG_L 28
+#define IMG_CH 1
+#define CLASS_NUM (10)
+char* labels[CLASS_NUM] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+#elif TEST_CIFAR
+#include "cifar10_q.h"
+#include "pic/pic2.h"
+#define IMG_L 32
+#define IMG_CH 3
+#define CLASS_NUM 10
+char* labels[CLASS_NUM] = {"airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "hrose", "ship", "truck"};
+
+#elif TEST_VWW
+#include "vww96_q.h"
+#include "pic/pic_person1.h"
+#define IMG_L 96
+#define IMG_CH 3
+#define CLASS_NUM 2
+char* labels[CLASS_NUM] = {"no-person", "person"};
+#endif
+
+volatile uint32_t g_us_tick;
+void TIM6_Init(void)  // 1us
+{
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure = {0};
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
+
+    TIM_TimeBaseStructure.TIM_Period = 1;
+    TIM_TimeBaseStructure.TIM_Prescaler = 144 - 1;
+    TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+
+    TIM_TimeBaseInit(TIM6, &TIM_TimeBaseStructure);
+    TIM_ITConfig(TIM6, TIM_IT_Update, ENABLE);
+    TIM_ClearITPendingBit(TIM6, TIM_IT_Update);
+    TIM_Cmd(TIM6, ENABLE);
+    NVIC_SetPriority(TIM6_IRQn, 0x80);
+    NVIC_EnableIRQ(TIM6_IRQn);
+}
+
 
 
 static tm_err_t layer_cb(tm_mdl_t* mdl, tml_head_t* lh)
@@ -92,14 +116,14 @@ static void parse_output(tm_mat_t* outs)
     float* data  = out.dataf;
     float maxp = 0;
     int maxi = -1;
-    for(int i=0; i<10; i++){
+    for(int i = 0; i < CLASS_NUM; i++){
         printf("%d: %.3f\n", i, data[i]);
         if(data[i] > maxp) {
             maxi = i;
             maxp = data[i];
         }
     }
-    TM_PRINTF("### Predict output is: Number %d, prob %.3f\n", maxi, maxp);
+    TM_PRINTF("### Predict output is: Class %d, Lable %s, prob %.3f\n", maxi, labels[maxi], maxp);
     return;
 }
 
@@ -119,13 +143,13 @@ int main(void) {
     tm_mdl_t mdl;
 
     for (int i = 0; i < 28 * 28; i++) {
-        TM_PRINTF("%3d,", mnist_pic[i]);
+        TM_PRINTF("%3d,", pic[i]);
         if (i % 28 == 27)
             TM_PRINTF("\n");
     }
 
-    tm_mat_t in_uint8 = { 3, 28, 28, 1, (mtype_t*) mnist_pic };
-    tm_mat_t in = { 3, 28, 28, 1, NULL };
+    tm_mat_t in_uint8 = {3,IMG_L,IMG_L,IMG_CH, (mtype_t*)pic};
+    tm_mat_t in = {3,IMG_L,IMG_L,IMG_CH, NULL};
     tm_mat_t outs[1];
     tm_err_t res;
     tm_stat((tm_mdlbin_t*) mdl_data);
